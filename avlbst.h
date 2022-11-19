6ab373833b910893177a89e35f0aa665587b4a58
Kevin Arackaparambil <arackapa@usc.edu>
2022-11-19 23:22:24 +0000

    HW4 Part 4 Submission

--- avlbst.h
+++ avlbst.h
@@ -138,9 +138,21 @@ protected:
     virtual void nodeSwap( AVLNode<Key,Value>* n1, AVLNode<Key,Value>* n2);
 
     // Add helper functions here
-
-
+    AVLNode<Key, Value>* conversion(Node<Key, Value>* n) const; 
+    void rightRotate(AVLNode<Key,Value>* n);
+    void leftRotate(AVLNode<Key,Value>* n);
+    void insertFix(AVLNode<Key, Value>* p, AVLNode<Key, Value>* n);
+    void removeFix(AVLNode<Key, Value>* n, int8_t diff);  
 };
+
+
+template<class Key, class Value>
+AVLNode<Key, Value> *AVLTree<Key, Value>::conversion(Node<Key, Value>* n) const
+{
+    return static_cast<AVLNode<Key, Value>*>(n);
+}
+
+
 
 /*
  * Recall: If key is already in the tree, you should 
@@ -150,7 +162,168 @@ template<class Key, class Value>
 void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
 {
     // TODO
+    // base case if tree is empty, set n as root, b(n) = 0 
+    if (this->root_ == NULL)
+    {
+      // std::cout << "empty tree and setting the root" << std::endl; 
+      AVLNode<Key, Value>* newNode = new AVLNode<Key, Value>(new_item.first, new_item.second, NULL); 
+      this->root_ = newNode; 
+    }
+    else
+    {
+      // if there is something in the tree, we need to compare the keys
+      // if less than go left, if greater than go right  
+      AVLNode<Key, Value>* traveler = conversion(this->root_);
+      
+      while (traveler != NULL) 
+      {
+        // go left since key is less than 
+        if (traveler->getKey() > new_item.first) 
+        {
+          // if there is nothing left, we create the node here and end the loop 
+          if (traveler->getLeft() == NULL)
+          {
+            // std::cout << "this is the left node being created" << std::endl; 
+            AVLNode<Key, Value>* newNode = new AVLNode<Key, Value>(new_item.first, new_item.second, traveler);
+            traveler->setLeft(newNode);
+            // adding a left node, we must increase the balance of parent by -1 
+            if (traveler->getBalance() == -1) traveler->setBalance(0); 
+            else if (traveler->getBalance() == 1) traveler->setBalance(0);  
+            else if (traveler->getBalance() == 0) 
+            {
+              traveler->setBalance(-1); 
+              insertFix(traveler, newNode);
+            }
+            break; 
+          }
+          traveler = traveler->getLeft(); 
+        }
+        // if key is the same, set the value 
+        else if (traveler->getKey() == new_item.first)
+        {
+          traveler->setValue(new_item.second);
+          break; 
+        }
+        //otherwise if the key is greater than, go right 
+        else if (traveler->getKey() < new_item.first)
+        {
+          if (traveler->getRight() == NULL)
+          {
+            AVLNode<Key, Value>* newNode = new AVLNode<Key, Value>(new_item.first, new_item.second, traveler); 
+            traveler->setRight(newNode); 
+            // adding a right node, we must increase balance of parent by 1 
+            if (traveler->getBalance() == -1) traveler->setBalance(0); 
+            else if (traveler->getBalance() == 1) traveler->setBalance(0);  
+            else if (traveler->getBalance() == 0) 
+            {
+              traveler->setBalance(1); 
+              insertFix(traveler, newNode);
+            }
+            break;
+          }
+          traveler = traveler->getRight(); 
+        }
+      }
+    }
 }
+
+template<class Key, class Value>
+void AVLTree<Key, Value>::insertFix(AVLNode<Key, Value>* p, AVLNode<Key, Value>* n)
+{
+  AVLNode<Key, Value>* g = p->getParent(); 
+
+  if (p == NULL || g == NULL) return; 
+  else if (g->getLeft() == p)
+  {
+    // std::cout << "we are in insert fix and left left case:::::" << std::endl;
+    g->updateBalance(-1);
+    // std::cout << "THIS IS THE BALANCE OF G" << g->getBalance() << std::endl; 
+    if (g->getBalance() == 0) return; 
+    else if (g->getBalance() == -1) insertFix(g, p);
+    else if (g->getBalance() == -2) 
+    {
+      // left left case 
+      if (p->getLeft() == n) 
+      {
+        // std::cout << "We should be in here for LEFT LEFT Case " << std::endl; 
+        p->setBalance(0);
+        g->setBalance(0); 
+        rightRotate(g); 
+      }
+      // left right case 
+      else if (p->getRight() == n)
+      {
+        leftRotate(p);
+        rightRotate(g);
+        if (n->getBalance() == -1)
+        {
+          p->setBalance(0);
+          g->setBalance(1); 
+          n->setBalance(0);
+        }
+        else if (n->getBalance() == 0)
+        {
+          p->setBalance(0); 
+          g->setBalance(0);
+          n->setBalance(0);
+        }
+        else if (n->getBalance() == 1)
+        {
+          p->setBalance(-1); 
+          g->setBalance(0);
+          n->setBalance(0);
+        }
+      }
+    }
+
+  }
+  else if (g->getRight() == p)
+  {
+    // std::cout << "we are in insert fix and right case:::::" << std::endl;
+    g->updateBalance(1);
+    // std::cout << "THIS IS THE BALANCE OF G" << g->getBalance() << std::endl; 
+    if (g->getBalance() == 0) return; 
+    else if (g->getBalance() == 1) insertFix(g, p);
+    else if (g->getBalance() == 2) 
+    {
+    // std::cout << "we are in insert fix and right case::::" << std::endl; 
+    // right right case 
+      if (p->getRight() == n) 
+      {
+        // std::cout << "righ right ZIG ZIG " << std::endl; 
+        leftRotate(g);
+        p->setBalance(0);
+        g->setBalance(0); 
+      }
+      // left right case 
+      else if (p->getLeft() == n)
+      {
+        rightRotate(p);
+        leftRotate(g);
+        if (n->getBalance() == 1)
+        {
+          p->setBalance(0);
+          g->setBalance(-1); 
+          n->setBalance(0);
+        }
+        else if (n->getBalance() == 0)
+        {
+          p->setBalance(0); 
+          g->setBalance(0);
+          n->setBalance(0);
+        }
+        else if (n->getBalance() == -1)
+        {
+          p->setBalance(1); 
+          g->setBalance(0);
+          n->setBalance(0);
+        }
+      }
+    }
+  }
+
+}
+
 
 /*
  * Recall: The writeup specifies that if a node has 2 children you
@@ -159,7 +332,264 @@ void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
 template<class Key, class Value>
 void AVLTree<Key, Value>:: remove(const Key& key)
 {
-    // TODO
+        // TODO
+    AVLNode<Key, Value>* n = conversion(this->internalFind(key));  
+
+    if (n == NULL) return; 
+
+    int8_t diff = 0; 
+    
+    if (n == this->root_)
+    {
+      delete n;
+      this->root_ = NULL; 
+      // std::cout << "are we able to delete or are we seggin" << std::endl;  
+      return; 
+    }
+
+    AVLNode<Key, Value>* p = n->getParent(); 
+
+    if (n->getLeft() != NULL && n->getRight() != NULL)
+    {
+        AVLNode<Key, Value>* n2 = conversion(this->predecessor(n));
+        // if (n ->getLeft() == n)
+        // {
+        //   n2
+        // }
+        nodeSwap(n, n2);
+        if (n == this->root_)
+        {
+          this->root_ = n2; 
+        }
+    }
+
+    if (p != NULL)
+    {
+        if (p->getLeft() == n)
+        {
+            diff = 1;
+            p->setLeft(NULL);
+        }
+        if (p->getRight() == n)
+        {
+            diff = -1; 
+            p->setRight(NULL); 
+        }
+    }
+
+    delete n; 
+    removeFix(p, diff); 
+}
+
+template<class Key, class Value>
+void AVLTree<Key, Value>::removeFix(AVLNode<Key, Value>* n, int8_t diff)
+{
+  if (n == NULL) return; 
+
+  int8_t ndiff = 0; 
+  AVLNode<Key, Value>* p = n->getParent(); 
+  if (p != NULL && p->getLeft() == n)
+  {
+    ndiff = 1; 
+  }
+  else if (p!= NULL && p->getRight() == n)
+  {
+    ndiff = -1;
+  }
+
+  if (diff == -1)
+  {
+    if (n->getBalance() + diff == -2)
+    {
+      AVLNode<Key, Value>* c = n->getLeft(); 
+      // zig zig case 
+      if (c->getBalance() == -1)
+      {
+        rightRotate(n); 
+        n->setBalance(0);
+        c->setBalance(0);
+        removeFix(p, ndiff);
+      }
+      else if (c->getBalance() == 0)
+      {
+        rightRotate(n); 
+        n->setBalance(-1);
+        c->setBalance(1);
+      }
+      else if (c->getBalance() == 1)
+      {
+        AVLNode<Key, Value>* g = c->getRight(); 
+        leftRotate(c);
+        rightRotate(n);
+        if (g->getBalance() == 1)
+        {
+          n->setBalance(0);
+          c->setBalance(-1);
+          g->setBalance(0);
+        }
+        else if (g->getBalance() == 0)
+        {
+          n->setBalance(0);
+          c->setBalance(0);
+          g->setBalance(0);
+        }
+        else if (g->getBalance() == -1)
+        {
+          n->setBalance(1);
+          c->setBalance(0);
+          g->setBalance(0);
+        }
+        removeFix(p, ndiff);
+      }
+    }
+    else if (n->getBalance() + diff == -1)
+    {
+      n->setBalance(-1);
+    }
+    else if (n->getBalance() + diff == 0)
+    {
+      n->setBalance(0);
+      removeFix(p, ndiff);
+    }
+  }
+  // this is the mirror case 
+  else if (diff == 1)
+  {
+    if (n->getBalance() + diff == 2)
+    {
+      AVLNode<Key, Value>* c = n->getRight(); 
+      // zig zig case 
+      if (c->getBalance() == 1)
+      {
+        leftRotate(n); 
+        n->setBalance(0);
+        c->setBalance(0);
+        removeFix(p, ndiff);
+      }
+      else if (c->getBalance() == 0)
+      {
+        leftRotate(n); 
+        n->setBalance(1);
+        c->setBalance(-1);
+      }
+      else if (c->getBalance() == -1)
+      {
+        AVLNode<Key, Value>* g = c->getLeft(); 
+        rightRotate(c);
+        leftRotate(n);
+        if (g->getBalance() == -1)
+        {
+          n->setBalance(0);
+          c->setBalance(1);
+          g->setBalance(0);
+        }
+        else if (g->getBalance() == 0)
+        {
+          n->setBalance(0);
+          c->setBalance(0);
+          g->setBalance(0);
+        }
+        else if (g->getBalance() == 1)
+        {
+          n->setBalance(-1);
+          c->setBalance(0);
+          g->setBalance(0);
+        }
+        removeFix(p, ndiff);
+      }
+    }
+    else if (n->getBalance() + diff == 1)
+    {
+      n->setBalance(1);
+    }
+    else if (n->getBalance() + diff == 0)
+    {
+      n->setBalance(0);
+      removeFix(p, ndiff);
+    }
+  }
+}
+
+
+template<class Key, class Value>
+void AVLTree<Key, Value>:: leftRotate(AVLNode<Key,Value>* n)
+{
+  AVLNode<Key,Value>* rightNode = n->getRight(); 
+  AVLNode<Key,Value>* p = n->getParent(); 
+  if (n == NULL) return; 
+  if (rightNode == NULL) return; 
+  
+  if (n == this->root_)
+  {
+    this->root_ = rightNode; 
+  }
+  else if (p->getLeft() == n)
+  {
+    p->setLeft(rightNode); 
+  }
+  else if (p->getRight() == n)
+  {
+    p->setRight(rightNode); 
+  }
+  n->setParent(rightNode); 
+  n->setRight(rightNode->getLeft()); 
+  if (rightNode->getLeft() != NULL)
+  {
+    rightNode->getLeft()->setParent(n); 
+  }
+  rightNode->setLeft(n);
+  rightNode->setParent(p);
+}
+
+
+
+template<class Key, class Value>
+void AVLTree<Key, Value>:: rightRotate(AVLNode<Key,Value>* n)
+{
+  AVLNode<Key,Value>* leftNode = n->getLeft(); 
+  AVLNode<Key,Value>* p = n->getParent(); 
+
+  // if we are rotating at the root level, we want to update the root of the tree 
+  if (n == NULL) return; 
+ 
+  // if n is the root that means there is no parent 
+
+  if (n == this->root_)
+  {
+    // std::cout << "N IS THE ROOT*** " << std::endl; 
+    this->root_ = leftNode; 
+  }
+  else if (p->getLeft() == n)
+  {
+    p->setLeft(leftNode); 
+  }
+  else if (p->getRight() == n)
+  {
+    p->setRight(leftNode); 
+  }
+
+  // std::cout << "MAKE IT HERE: 1" << std::endl; 
+  // shifting node n to the right and updating its parent to the left node 
+  n->setParent(leftNode); 
+    // std::cout << "MAKE IT HERE: 2" << std::endl; 
+
+  // shifting over the left right subtree to the right tree 
+  n->setLeft(leftNode->getRight()); 
+    // std::cout << "MAKE IT HERE: 3" << std::endl; 
+
+  if (leftNode->getRight() != NULL)
+  {
+    leftNode->getRight()->setParent(n); 
+  }
+    // std::cout << "MAKE IT HERE: 4" << std::endl; 
+
+  // updating left node as the central node 
+  leftNode->setRight(n);
+    // std::cout << "MAKE IT HERE: 5" << std::endl; 
+
+  leftNode->setParent(p); 
+    // std::cout << "MAKE IT HERE: 6" << std::endl; 
+
 }
 
 template<class Key, class Value>
